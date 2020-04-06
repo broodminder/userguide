64e59809714b3d8e2d1b55d6a632f3809c0075c2
mickael2407 <pezzoni.mickael@gmail.com>
2020-04-06 17:36:39 +0200

    change build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 docker container exec -ti doc mkdocs build --clean
 rm -Rrv /var/www/html/doc/*
-cp -Rrv /home/mickael/userguide/site/* /var/www/html/doc/
+cp -Rrv /apiwatch/production/userguide/site/* /var/www/html/doc/
