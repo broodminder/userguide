28329f4c6ce7f754f6db77ff869b4866f666a10e
mickael2407 <pezzoni.mickael@gmail.com>
2020-04-06 19:39:39 +0200

    fix script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 docker run -d -p 8000:8000 --name doc -v /apiwatch/production/userguide/:/docs squidfunk/mkdocs-material
 docker container exec -ti doc mkdocs build --clean
-rm -Rrv /var/www/html/doc/*
+rm -Rrfv /var/www/html/doc/*
 cp -Rrv /apiwatch/production/userguide/site/* /var/www/html/doc/
 sudo docker container rm -f doc
