4eef1125ff0823e6d83eed5a49c6d419655f2914
Lorenzo <lorenzo@mellisphera.com>
2020-04-15 18:28:17 +0200

    edition build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run -d -p 8000:8000 --name doc -v /apiwatch/production/userguide/:/docs squidfunk/mkdocs-material
 docker container exec -ti doc mkdocs build --clean
-rm -Rrfv /var/www/html/doc/*
-cp -Rrv /apiwatch/production/userguide/site/* /var/www/html/doc/
+rm -Rrf /var/www/html/doc/*
+cp -Rr /apiwatch/production/userguide/site/* /var/www/html/doc/
 sudo docker container rm -f doc
