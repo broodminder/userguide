c65400a2d8287e8574beb9063a0946b87cad2d66
Lorenzo <lorenzo@mellisphera.com>
2020-05-19 08:12:02 +0200

    ajout build fr

--- build---to-review.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -d -p 8000:8000 --name doc -v /apiwatch/production/userguide/:/docs squidfunk/mkdocs-material
-docker container exec -ti doc mkdocs build --clean
-rm -Rrf /var/www/html/doc/*
-cp -Rr /apiwatch/production/userguide/site/* /var/www/html/doc/
-sudo docker container rm -f doc
